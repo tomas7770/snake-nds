02b373e5e8cf95f9a355f7e749396ce46e4ca074
tomas7770 <77364520+tomas7770@users.noreply.github.com>
2023-09-06 14:19:13 +0000

    Add an actual title to the title screen

--- source/title.c
+++ source/title.c
@@ -1,9 +1,11 @@
 #include <nf_lib.h>
+#include <nds.h>
 #include "title.h"
 #include "global.h"
 #include "game.h"
 
-#define TEXT_LAYER 0
+#define TEXT_LAYER 1
+#define TITLE_LAYER 0
 
 #define SPRITE_ARROW 0
 #define PALETTE_ARROW 0
@@ -15,6 +17,50 @@ const char title_screen = 0;
 
 Difficulty selected_difficulty = NORMAL;
 bool selected_wrap = true;
+
+// Wave effect variables
+s16 bgx[192];       // Horizontal scroll of each line
+s8 i[192];          // Scroll speed of each line
+
+// Function that runs after a scanline is drawn. By modifying the values of the
+// scroll registers it's possible to add a wave effect.
+void hblank_handler()
+{
+    u32 vline = REG_VCOUNT; // Get the current line
+
+    if (vline >= 20 && vline <= 50)
+    {
+        // If this is a line inside the desired screen area, handle the effect
+        bgx[vline] += i[vline];
+
+        if ((bgx[vline] < 1) || (bgx[vline] > 63))
+            i[vline] *= -1;
+
+        NF_ScrollBg(title_screen, TITLE_LAYER, (bgx[vline] / 8) - 4, 0);
+    }
+}
+
+void init_wave_effect() {
+    // Initialize the wave effect
+    s8 inc = 1;
+    int x = 0;
+
+    for (int y = 0; y < 192; y++)
+    {
+        x += inc;
+        bgx[y] = x;
+
+        if ((x < 1) || (x > 63))
+            inc *= -1;
+
+        i[y] = inc;
+    }
+
+    // Register a function to be called after a screen line has been drawn (when
+    // the horizontal blanking period starts)
+    irqSet(IRQ_HBLANK, hblank_handler);
+    irqEnable(IRQ_HBLANK);
+}
 
 // Update position of difficulty selection arrow
 void update_arrow_position() {
@@ -53,6 +99,9 @@ void init_title() {
     NF_WriteText(title_screen, TEXT_LAYER, 12, 17, "<Select>");
     update_wrap_text();
 
+    NF_LoadTiledBg("title", "title", 256, 256);
+    NF_CreateTiledBg(title_screen, TITLE_LAYER, "title");
+
     // Init sprites
     NF_InitSpriteSys(title_screen);
     NF_LoadSpriteGfx("arrow", SPRITE_ARROW, 8, 8);
@@ -62,6 +111,8 @@ void init_title() {
     NF_CreateSprite(title_screen, SPRITE_ARROW, SPRITE_ARROW, PALETTE_ARROW, 0, 0);
 
     update_arrow_position();
+
+    init_wave_effect();
 }
 
 void tick_title() {
@@ -79,6 +130,8 @@ void tick_title() {
         update_wrap_text();
     }
     if (keys & (KEY_A | KEY_START)) {
+        irqDisable(IRQ_HBLANK);
+        irqClear(IRQ_HBLANK);
         init_game(selected_difficulty, selected_wrap);
     }
 }
